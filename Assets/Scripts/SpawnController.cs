ba621a1285a42ec0a771f18d3dffc02768afb117
Roosevelt Boyland III <treyboyland@aol.com>
2025-06-18 12:18:32 -0500

    Create SpawnController.cs

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,28 @@
+public class SpawnController
+{
+int currentRound;
+
+List<EnemySpawnData> enemies;
+
+[SerializeField]
+float secondsBetweenSpawnGroups;
+
+[SerializeField]
+float secondsBetweenSpawns;
+
+float spawnTimer = 0;
+
+bool canSpawn;
+
+static Player player;
+
+
+[Serializable]
+public struct EnemySpawnData
+{
+public Enemy Enemy;
+public Vector2Int RoundRestriction;
+}
+
+
+}
